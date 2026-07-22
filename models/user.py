8c95022d948892d449db12498a55f80d19999b4c
Neethu Sajeeve <162826616+Nneethu@users.noreply.github.com>
2026-07-23 00:01:35 +0530

    Add files via upload

--- models/user.py
+++ models/user.py
@@ -10,9 +10,18 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
     full_name: Mapped[str] = mapped_column(String(150))
+
     email: Mapped[str] = mapped_column(String(255), unique=True)
+
     password_hash: Mapped[str] = mapped_column(String(255))
+
     role: Mapped[str] = mapped_column(String(50))
+
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
